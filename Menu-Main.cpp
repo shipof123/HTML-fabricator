e2e1e116d2130ddf21f682ee82c259927ad77c2c
shipof123 <34876781+shipof123@users.noreply.github.com>
2018-03-27 10:07:06 -0500

    Update Menu-Main.cpp

--- Menu-Main.cpp
+++ Menu-Main.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
 #include <stdio.h>
-#include "html.h"
+/*#include "html.h"     //includes from other headers
+*/
+void cls()
+{
+    for(int i=1;i<26;i++)
+    printf("\n");
+}
 int main()
 {
-    
+    using namespace std;
+    cout<<"WELCOME to REALLY BAD PROGRAM Shipof MADE!!!!"<<endl;
+    cout <<"--------------------------------------------"<<endl;
+    char answer;
+    while(1)
+    {
+        cout<<"Please select an option "<<endl;
+        cout<<"\t\t1-Create new project"<<endl;
+        cout<<"\t\t2-Append old project"<<endl;
+        cout<<"\t\t3-Quir"<<endl;
+        cin>>answer;
+        switch(answer)
+            case "1":
+                //function call
+            case "2";
+                //function call
+            case "3":
+                break;
+        default:
+        cls();
+    }
     return 0;
 }
