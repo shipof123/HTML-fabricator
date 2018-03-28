bf4a04eb1805c29ec3befba4700b112129c38c52
shipof123 <34876781+shipof123@users.noreply.github.com>
2018-03-28 10:35:42 -0500

    Update test.h

--- test.h
+++ test.h
@@ -1,11 +1,12 @@
 #ifndef TEST_H_
 #define TEST_H_
-#include <stdio.h>
+#include "includes.h"
 namespace test 
 {
 int test()
 {
     html::br();
+    file::file();
     printf("WORKS\n");
     return 0;
 }
