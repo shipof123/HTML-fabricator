c2460438f1b491974f22599d740d4c6806bfe76d
shipof123 <34876781+shipof123@users.noreply.github.com>
2018-03-29 08:56:10 -0500

    Add files via upload

---
+++ nfile.h
@@ -0,0 +1,44 @@
+#ifndef NFILE_H_
+#define NFILE_H_
+#include <string>
+#include <fstream>
+#include <iostream>
+using namespace std;
+namespace nfile
+{
+int nfile()
+{
+	int choice2;
+	int thing2 =0;
+	string header;
+	string filename;
+	cout <<"What would you like to name your file?\n";
+	cout << "(don't put in the .html)\n";
+	cin >> filename;
+	string cssfile = filename;
+	filename = filename +".html";
+	ofstream outf(filename.c_str());
+	outf << "<!DOCTYPE html>\n<html>\n<head><link rel=\"stylesheet\" href=\""<<cssfile<<".css\">\n<center><h1>"<< header << "<left></h1></head>\n<body>"<<endl;
+		while(thing2=0)
+		{
+			std::cout<<"Would you like to add a ...."<<endl;
+			std::cout<<"1\t..a link"<<endl;
+			std::cout<<"2\t..an image"<<endl;
+			std::cout<<"3\t..a table "<<endl;
+			//std::cout<<"4\t.."<<endl;
+			std::cout<<"5\tquit"<<endl;
+			cin>>choice2;
+			if (choice2==5)
+		{
+			thing2++;
+		}
+		}
+	while(thing2!=1)
+	{
+	thing2++;
+	}
+return 0;
+}
+
+}
+#endif 
---
+++ ofile.h
@@ -0,0 +1,35 @@
+#ifndef OFILE_H_
+#define OFILE_H_
+#include <string>
+#include <fstream>
+#include <iostream>
+using namespace std;
+namespace ofile
+{
+	int ofile()
+	{
+		string filename;
+		int choice2;
+		int thing2 =0;
+		printf("Enter filename(w/o .html)\n");
+		cin >>filename;
+		filename=filename+".html";
+		while(thing2<1)
+		{
+			std::cout<<"Would you like to add a ...."<<endl;
+			std::cout<<"1\t..a link"<<endl;
+			std::cout<<"2\t..an image"<<endl;
+			std::cout<<"3\t..a table "<<endl;
+			//std::cout<<"4\t.."<<endl;
+			std::cout<<"5\tquit"<<endl;
+			cin>>choice2;
+			if (choice2==5)
+			{
+				thing2++;
+			}
+			
+		}
+			return 0;
+	}
+}
+#endif
